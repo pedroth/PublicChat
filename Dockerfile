4afdf8ceab0fa59e70095206f18cd03285196bbd
pedroth <pedrotiago92@gmail.com>
2026-02-18 16:24:35 +0000

    Update Dockerfile and updateDocker.sh for cross-platform builds and versioning

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
-FROM alpine/git AS clone
+ARG BUILDPLATFORM
+
+FROM --platform=$BUILDPLATFORM alpine/git AS clone
 WORKDIR /app
 RUN git clone https://github.com/pedroth/PublicChat.git
 
-FROM maven:3.9.6-eclipse-temurin-17-alpine AS build
+FROM --platform=$BUILDPLATFORM maven:3.9.9-eclipse-temurin-17 AS build
 WORKDIR /app
 COPY --from=clone /app/PublicChat/ /app
 RUN sh scripts/install.sh
 
-FROM eclipse-temurin:17-jre-alpine
+FROM eclipse-temurin:17-jre
 WORKDIR /app
 COPY --from=build /app/PublicChatServer /app
 CMD ["sh", "run.sh"]
--- updateDocker.sh
+++ updateDocker.sh
@@ -1,10 +1,9 @@
 # This should be ran after everything is in git
-VERSION=v2.0.0
-docker build -t pedroth/public-chat:latest -t pedroth/public-chat:$VERSION .
-docker push pedroth/public-chat:latest
-docker push pedroth/public-chat:$VERSION
+# VERSION=v2.0.1
+# docker build -t pedroth/public-chat:latest -t pedroth/public-chat:$VERSION .
+# docker push pedroth/public-chat:latest
+# docker push pedroth/public-chat:$VERSION
 
-#Arm
-#VERSION=linux-arm-v6_v2
-#docker build -t pedroth/public-chat:$VERSION .
-#docker push pedroth/public-chat:$VERSION
+#Arm (cross-build from amd64)
+VERSION=linux-arm-v7_v2
+docker buildx build --platform linux/arm/v7 -t pedroth/public-chat:$VERSION --push .
